c17da8eefd9107e7cf02112d3d866cf65af9b5cc
Lauv <pussaaon@gmail.com>
2024-10-16 09:08:10 +0700

    add theme mode

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { ModeToggle } from '@/components/ui/mode-toggle';
+import React from 'react';
+
+function RootLayout () {
+	return (
+		<>
+			<div>RootLayout </div>
+			<ModeToggle />
+		</>
+	)
+}
+
+export default RootLayout 
